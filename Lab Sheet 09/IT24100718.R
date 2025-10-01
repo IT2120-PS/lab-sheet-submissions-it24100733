b419fee4aeb79ade07ef5d385de077d5f8bb1e63
IT24100733 <it24100733@my.sliit.lk>
2025-10-01 22:24:33 +0530

    Delete Lab Sheet 09/IT24100718.R

--- Lab Sheet 09/IT24100718.R
+++
@@ -1,9 +0,0 @@
-setwd("//Users//Malindu//Documents//Cmps Lecture Slides//LAB 9//Lab 9")
-##Exercise
-#1).
-bake <- rnorm(25, mean = 45, sd = 2)
-bake
-
-#2).
-t.test(bake, mu = 46, alternative = "less")
-
